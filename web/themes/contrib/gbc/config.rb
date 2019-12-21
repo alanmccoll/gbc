0a596bf6419e064102971611e66e11bf8e35935f
alanmccoll <alan@itrva.com>
2019-12-21 05:51:50 -0500

    initial commit

---
+++ web/themes/contrib/gbc/config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+require 'compass/import-once/activate'
+# Set this to the root of your project when deployed:
+http_path = '/themes/contrib/gbc'
+css_dir = 'css'
+sass_dir = 'assets/sass'
+images_dir = 'images'
+fonts_dir = 'bootstrap/fonts'
+javascripts_dir = 'bootstrap/javascripts'
+
+# You can select your preferred output style
+# here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+
+# To enable relative paths to assets via
+# compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the
+# original location of your selectors. Uncomment:
+# line_comments = false
+
+# Comment this out to default to SCSS:
+# preferred_syntax = :sass
